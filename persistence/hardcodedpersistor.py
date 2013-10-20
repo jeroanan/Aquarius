668405c45363fce903499dbd02020f7be2c12839
jeroanan <david.tosslehoff@gmail.com>
2013-10-20 14:15:39 +0100

    Added hardcoded persistor

---
+++ persistence/hardcodedpersistor.py
@@ -0,0 +1,26 @@
+from persistence.persistor import persistor
+from book import book
+
+class hardcodedpersistor(persistor):
+    
+    def __init__(self):
+        self.__data = []
+        b = book()
+        b.Title = "book"
+        b.Id = 1
+        self.__data.append(b)
+        
+    def SearchBooks(self, searchTerm):
+        for book in self.__data:
+            if searchTerm in book.Title and searchTerm != "":
+                yield book
+                
+    def ListBooksByFirstLetter(self, firstLetter):
+        for book in self.__data:
+            if book.Title.startswith(firstLetter):
+                yield book
+                
+    def GetBookDetails(self, bookId):
+        for book in self.__data:
+            if book.Id == bookId:
+                return book
