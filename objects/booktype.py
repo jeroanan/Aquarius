b87423279c96368f13875149529eef43ac80ea2a
jeroanan <david.tosslehoff@gmail.com>
2013-10-25 23:32:17 +0100

    finished off bookformat object

--- objects/booktype.py
+++ objects/booktype.py
@@ -1,2 +1,21 @@
 class booktype(object):
-    pass
+    
+    __format = ""
+    __mimetype = ""
+    
+    @property
+    def Format(self):
+        return self.__format
+    
+    @Format.setter
+    def Format(self, value):
+        self.__format = value
+        
+    @property
+    def MimeType(self):
+        return self.__mimetype
+    
+    @MimeType.setter
+    def MimeType(self, value):
+        self.__mimetype = value
+        
--- objects/tests/BookType_Tests.py
+++ objects/tests/BookType_Tests.py
@@ -4,5 +4,10 @@ from objects.booktype import booktype
 
 class BookType_Tests(unittest.TestCase):
     
-    def testCanInitialise(self):
-        booktype()
+    def testHasFormatCode(self):
+        bookType = booktype()
+        self.assertEqual(True, hasattr(bookType, "Format"))
+        
+    def testHasMimeType(self):
+        bookType = booktype()
+        self.assertEqual(True, hasattr(bookType, "MimeType"))
