fbb913e653fa4511165620d34b9adffa264df019
Dave Wilson <david.tosslehoff@gmail.com>
2013-11-04 23:12:54 +0000

    uppded test coverage for output factory

--- output/tests/OutputFactory_Tests.py
+++ output/tests/OutputFactory_Tests.py
@@ -2,6 +2,8 @@
 
 import unittest
 from output.outputfactory import outputfactory
+from output.web.web import web
+from output.console.console import console
 
 class OutputFactory_Tests(unittest.TestCase):
     
@@ -12,7 +14,12 @@ class OutputFactory_Tests(unittest.TestCase):
         self.__f = outputfactory(app, config)
 
     def testFactoryGivesConsoleOutputByDefault(self):
-        self.__f.GetOutput("")
-    
+        o = self.__f.GetOutput("")
+        self.assertIsInstance(o, console)
+        
+    def testGetWebOutput(self):
+        o = self.__f.GetOutput("web")
+        self.assertIsInstance(o, web)
+        
 if __name__=="__main__":
     unittest.main()
