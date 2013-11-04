c398bb5c84aa6d500b020470c566f5c51c437cb7
Dave Wilson <david.tosslehoff@gmail.com>
2013-11-04 22:08:37 +0000

    Added mock config class

--- persistence/tests/PersistenceFactory_Tests.py
+++ persistence/tests/PersistenceFactory_Tests.py
@@ -4,18 +4,22 @@ import unittest
 from persistence.persistencefactory import persistencefactory
 from persistence.hardcodedpersistence.hardcodedpersistence import hardcodedpersistence
 from persistence.sqlitepersistence.sqlitepersistence import sqlitepersistence
-from config import config
 
 class PersistenceFactory_Tests(unittest.TestCase):
     
     def setUp(self):
-        self.f = persistencefactory(config())        
+        self.f = persistencefactory(config_mock())        
         
     def testFactoryGivesHardcodedPersistorByDefault(self):
         self.assertIsInstance(self.f.GetPersistence("anyoldthing"), hardcodedpersistence)
         
     def testFactoryGivesSqlitePersistorWhenAsked(self):
         self.assertIsInstance(self.f.GetPersistence("sqlite"), sqlitepersistence)
+        
+class config_mock(object):
+    
+    def __init__(self):
+        self.SqlLiteDatabasePath = "./database.db"
         
 if __name__=="__main__":
     unittest.main()
