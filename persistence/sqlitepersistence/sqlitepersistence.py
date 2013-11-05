b327f959b3c78fae805f187ad9fc51111eace2ec
Dave Wilson <david.tosslehoff@gmail.com>
2013-11-05 23:09:58 +0000

    Cleanup

--- persistence/sqlitepersistence/sqlitepersistence.py
+++ persistence/sqlitepersistence/sqlitepersistence.py
@@ -5,15 +5,21 @@ class sqlitepersistence(object):
     def __init__(self, config):
         self.__config = config
         self.__createdb()
-    
+        
     def __createdb(self):
         conn = sqlite3.connect(self.__config.SqlLiteDatabasePath)
-        c = conn.cursor()
+        c = conn.cursor()        
         
-        with open("persistence/sqlitepersistence/createdb.sql") as f:
-            batch = f.read()
-        
-        for statement in batch.split(";"):
-            c.execute(statement)
+        sql = self.GetContentsOfDatabaseScript()        
+        self.ExecuteContentsOfDatabaseScript(c, sql)
             
         conn.close()
+        
+    def GetContentsOfDatabaseScript(self):
+        with open("persistence/sqlitepersistence/createdb.sql") as f:
+            batch = f.read()
+        return batch
+    
+    def ExecuteContentsOfDatabaseScript(self, c, batch):
+        for statement in batch.split(";"):
+            c.execute(statement)
