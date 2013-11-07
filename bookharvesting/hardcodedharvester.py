dbafc699ed8f38300a37a9d4d377eebbed6de93a
Dave Wilson <david.tosslehoff@gmail.com>
2013-11-07 20:29:01 +0000

    +hardcodedharvester

--- bookharvesting/hardcodedharvester.py
+++ bookharvesting/hardcodedharvester.py
@@ -1,11 +1,27 @@
+from objects.book import book
+from objects.bookformat import bookformat
+
 class hardcodedharvester(object):
     
     def __init__(self, app):
         self.__app = app
     
     def DoHarvest(self):
-        pass
+        self.__app.AddBook(self.__GetTestBook())        
         
+    def __GetTestBook(self):
+        b = book()
+        b.Author = "J. R. Hartley"
+        b.Title = "Fly Fishing"
+        b.Formats = [self.__GetTestFormat()]
+        return b
+
+    def __GetTestFormat(self):
+        f = bookformat()
+        f.Format = "EPUB"
+        f.Location = "/tmp/test.epub"
+        return f
+
         
     
     
--- bookharvesting/tests/hardcodedharvester_tests.py
+++ bookharvesting/tests/hardcodedharvester_tests.py
@@ -5,12 +5,30 @@ import unittest
 
 class hardcodedharvester_tests(unittest.TestCase):
     
+    def setUp(self):
+        self.__a = self.__app()    
+
     def test_DoHarvest(self):
-        h = hardcodedharvester(self.__app)
-        h.DoHarvest()
+        h = hardcodedharvester(self.__a)
+        h.DoHarvest()      
+        self.__CheckBook()
+        self.__CheckFormat()
         
-        
+    def __CheckBook(self):
+        self.assertEqual(1, len(self.__a.books))
+        self.assertEqual("J. R. Hartley", self.__a.books[0].Author)
+        self.assertEqual("Fly Fishing", self.__a.books[0].Title)
+    
+    def __CheckFormat(self):
+        self.assertEqual(1, len(self.__a.books[0].Formats))
+        self.assertEqual("EPUB", self.__a.books[0].Formats[0].Format)
+        self.assertEqual("/tmp/test.epub", self.__a.books[0].Formats[0].Location)
+                
     class __app(aquarius):
         
-        def AddBook(self):
-            pass
+        def __init__(self):
+            self.books = []
+        
+        def AddBook(self, book):
+            self.books.append(book)
+            
