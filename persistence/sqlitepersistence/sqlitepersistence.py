d273aa05e11aaa096aaa54751538dc52f57babd7
Dave Wilson <david.tosslehoff@gmail.com>
2013-11-20 23:31:52 +0000

    bit of a cleanup for sqlitepersistence

--- persistence/sqlitepersistence/sqlitepersistence.py
+++ persistence/sqlitepersistence/sqlitepersistence.py
@@ -8,38 +8,42 @@ class sqlitepersistence(object):
         
     def __createdb(self):
         conn = sqlite3.connect(self.__config.SqlLiteDatabasePath)
-        c = conn.cursor()        
-        
-        sql = self.__GetContentsOfDatabaseScript()        
-        self.__ExecuteContentsOfDatabaseScript(c, sql)
-            
+        c = conn.cursor()                
+        self.__ExecuteDatabaseScript()            
         conn.close()
         
     def AddBook(self, book):
         sql = "INSERT INTO Book (Title) VALUES ('%s')" % book.Title        
-        conn = sqlite3.connect(self.__config.SqlLiteDatabasePath)
-        c = conn.cursor()
-        c.execute(sql)
-        conn.commit()
-        conn.close()
+        self.__executeSql(sql)
         
     def SearchBooks(self, searchTerm):
         searchTerm = "%s%s%s" % ("%", searchTerm, "%")
         sql = "SELECT * FROM Book WHERE TITLE LIKE '%s';" % searchTerm
+        r = self.__executeSqlFetchAll(sql)
+        return r
+    
+    def __executeSql(self, sql):
+        self.__executeSqlFetchAll(sql)
+    
+    def __executeSqlFetchAll(self, sql):
         conn = sqlite3.connect(self.__config.SqlLiteDatabasePath)
         c = conn.cursor()
         r = c.execute(sql).fetchall()
+        conn.commit()
         conn.close()
         return r
-    
+        
+    def __ExecuteDatabaseScript(self):
+        batch = self.__GetContentsOfDatabaseScript()
+        for statement in batch.split(";"):
+            self.__executeSql(statement)
+            
     def __GetContentsOfDatabaseScript(self):
         with open("persistence/sqlitepersistence/createdb.sql") as f:
             batch = f.read()
         return batch
     
-    def __ExecuteContentsOfDatabaseScript(self, c, batch):
-        for statement in batch.split(";"):
-            c.execute(statement)
+    
 
     
     
