49591e857ecc087e099b249da5296e0aa8d08563
Dave Wilson <david.tosslehoff@gmail.com>
2013-11-25 22:25:28 +0000

    Added case-insensitivity

--- bookformats/bookfactory.py
+++ bookformats/bookfactory.py
@@ -1,10 +1,9 @@
-from objects.book import book
 from bookformats.epub import epub
 
 class bookfactory(object):
     
     def GetBook(self, filepath):        
-        if filepath.endswith(".epub"):
+        if filepath.lower().endswith(".epub"):
             return epub(filepath).Load()
 
 
--- bookformats/tests/bookfactory_tests.py
+++ bookformats/tests/bookfactory_tests.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import unittest
 from bookformats.bookfactory import bookfactory
 from objects.book import book
@@ -6,11 +8,19 @@ class bookfactory_tests(unittest.TestCase):
     
     def setUp(self):
         self.__f = bookfactory()
+        shutil.copy("bookformats/tests/data/TreasureIsland.epub", "bookformats/tests/data/TreasureIsland.EPUB")
+        
+    def tearDown(self):
+        os.remove("bookformats/tests/data/TreasureIsland.EPUB")
         
     def testGetBookEpubGetsBook(self):
         b = self.__f.GetBook("bookformats/tests/data/TreasureIsland.epub")
         self.assertIsInstance(b, book)       
     
+    def testGetBookEpubGetsBookUpperCase(self):
+        b = self.__f.GetBook("bookformats/tests/data/TreasureIsland.EPUB")
+        self.assertIsInstance(b, book)
+        
     def testGetBookUnrecognisedGetsNoBook(self):
         b = self.__f.GetBook("MyBook.rubbish")
         self.assertIsNone(b)
