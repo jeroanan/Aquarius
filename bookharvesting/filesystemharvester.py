1ad1f656421bb1cb83ca2e7db7eaeef3e638fae5
Dave Wilson <david.tosslehoff@gmail.com>
2013-11-25 21:44:57 +0000

    Extract till you drop\!

--- bookharvesting/filesystemharvester.py
+++ bookharvesting/filesystemharvester.py
@@ -7,12 +7,15 @@ class filesystemharvester(object):
         
     def harvest(self, path):
         for (path, dirs, files) in os.walk(path):
-            self.__getFilesFromPath(path, files)    
+            self.__getFilesFromPath(path, files)
 
     def __getFilesFromPath(self, path, files):
         if self.__pathContainsFiles(files):
-            for afile in files:                
-                self.__app.AddBook(afile)
+            self.__AddBook(files)
 
     def __pathContainsFiles(self, files):
         return len(files)>0
+    
+    def __AddBook(self, files):
+        for afile in files:
+            self.__app.AddBook(afile)
