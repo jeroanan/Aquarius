32432d892f2bc1e30bd62225a01d535a60ea960f
Dave Wilson <david.tosslehoff@gmail.com>
2013-12-11 22:11:51 +0000

    Reordered functions

--- bookformats/epub.py
+++ bookformats/epub.py
@@ -14,10 +14,6 @@ class epub(object):
         self.__setBookDetails()        
         return self.__book
 
-    def __setBookDetails(self):        
-        self.__book.Title = self.__getTextFromFirstTag(self.__bookMetaData, "title")
-        self.__book.Author = self.__getTextFromFirstTag(self.__bookMetaData, "creator")
-    
     def __getBookMetaData(self):
         with self.__zipFile.open(self.__getRootFilePath()) as f:
             self.__bookMetaData = f.read()
@@ -26,13 +22,17 @@ class epub(object):
         container = self.__getContainerFileContent()        
         return self.__getAttributeFromFirstTag(container, "rootfile", "full-path")
                 
+    def __getAttributeFromFirstTag(self, filecontent, tag, attribute):  
+        return self.__getFirstTag(filecontent, tag).get(attribute)
+    
     def __getContainerFileContent(self):        
         with self.__zipFile.open("META-INF/container.xml") as f:
             content = f.read()
         return content
-    
-    def __getAttributeFromFirstTag(self, filecontent, tag, attribute):  
-        return self.__getFirstTag(filecontent, tag).get(attribute)
+            
+    def __setBookDetails(self):        
+        self.__book.Title = self.__getTextFromFirstTag(self.__bookMetaData, "title")
+        self.__book.Author = self.__getTextFromFirstTag(self.__bookMetaData, "creator")
         
     def __getTextFromFirstTag(self, filecontent, tag):
         return self.__getFirstTag(filecontent, tag).text
