b6cb26ebae3d5d2c37bba2799c9fa1a1543c7ebd
Dave Wilson <david.tosslehoff@gmail.com>
2013-12-14 23:46:51 +0000

    Test cases now pass for books that have all three supported formats

--- output/web/requesthandlers/tests/htmlrequesthandlerbook_tests.py
+++ output/web/requesthandlers/tests/htmlrequesthandlerbook_tests.py
@@ -35,40 +35,54 @@ class htmlrequesthandlerbook_tests(unittest.TestCase):
         doc = self.__GetXmlFromBookHandlerForBookWithAllFormats()
         a = doc.findall("./body/div[@class='bookauthor']")[0]
         self.assertTrue(a.text.endswith(self.__testBookAuthor))
-        
-    def testBookHandlerHtmlDocumentContainsDownloadSection(self):
-        doc = self.__GetXmlFromBookHandlerForBookWithAllFormats()
-        a = doc.findall("./body/div[@class='downloads']")
-        self.assertEquals(1, len(a))
-        
+
     def testBookHandlerHtmlDocumentDownloadSectionEpubDownloadAnchorTagContainsCorrectHref(self):
         self.__assertFormatDownloadSectionAnchorTagHasCorrectDestination("epubdownload", "EPUB")
     
-    def testBookHandlerHtmlDocumentDownloadSectionEpubDownloadAnchorTagContainsCorrectTitleAttribute(self):
-        doc = self.__GetXmlFromBookHandlerForBookWithAllFormats()
-        xp = "./body/div[@class='downloads']/p[@class='epubdownload']/a[@title='Download %s in EPUB Format']" 
-        h =  xp % self.__testBookTitle
-        d = doc.findall(h)
-        self.assertEquals(1, len(d))
+    def testBookHandlerHtmlDocumentDownloadSectionEpubDownloadAnchorTagContainsCorrectTitle(self):
+        self.__assertFormatDownloadSectionAnchorTagHasCorrectTitle("epubdownload", "EPUB")    
         
     def testBookHandlerHtmlDocumentDownloadSectionEpubDownloadAnchorTagContainsCorrectText(self):
-        doc = self.__GetXmlFromBookHandlerForBookWithAllFormats()
-        t = doc.findall("./body/div[@class='downloads']/p[@class='epubdownload']/a")[0].text
-        self.assertEquals("EPUB", t)
+        self.__assertFormatDownloadSectionAnchorTagHasCorrectText("epubdownload", "EPUB")
         
     def testBookHandlerHtmlDocumentDownloadSectionMobiDownloadAnchorTagContainsCorrectHref(self):
         self.__assertFormatDownloadSectionAnchorTagHasCorrectDestination("mobidownload", "MOBI")
         
+    def testBookHandlerHtmlDocumentDownloadSectionMobiDownloadAnchorTagContainsCorrectTitle(self):
+        self.__assertFormatDownloadSectionAnchorTagHasCorrectTitle("mobidownload", "MOBI")       
+    
+    def testBookHandlerHtmlDocumentDownloadSectionMobiDownloadAnchorTagContainsCorrectText(self):
+        self.__assertFormatDownloadSectionAnchorTagHasCorrectText("mobidownload", "MOBI")
+                
+    def testBookHandlerHtmlDocumentDownloadSectionPdfDownloadAnchorTagContainsCorrectHref(self):
+        self.__assertFormatDownloadSectionAnchorTagHasCorrectDestination("pdfdownload", "PDF")
+        
+    def testBookHandlerHtmlDocumentDownloadSectionPdfDownloadAnchorTagContainsCorrectTitle(self):
+        self.__assertFormatDownloadSectionAnchorTagHasCorrectTitle("pdfdownload", "PDF")
+        
+    def testBookHandlerHtmlDocumentDownloadSectionPdfDownloadAnchorTagContainsCorrectText(self):
+        self.__assertFormatDownloadSectionAnchorTagHasCorrectText("pdfdownload", "PDF")
+    
+    def __assertFormatDownloadSectionAnchorTagHasCorrectTitle(self, sectionClass, formatCode):
+        doc = self.__GetXmlFromBookHandlerForBookWithAllFormats()
+        xp = "./body/div[@class='downloads']/p[@class='%s']/a[@title='Download %s in %s Format']" 
+        h =  xp % (sectionClass, self.__testBookTitle, formatCode)
+        self.__assertExecutingXPathGetsOneElement(doc, h)
+    
     def __assertFormatDownloadSectionAnchorTagHasCorrectDestination(self, sectionClass, formatCode):
         doc = self.__GetXmlFromBookHandlerForBookWithAllFormats()
         xp = "./body/div[@class='downloads']/p[@class='%s']/a[@href='/download?bookId=1&bookFormat=%s']"
         h = xp % (sectionClass, formatCode)
-        d = doc.findall(h)
-        self.assertEquals(1, len(d))
-            
+        self.__assertExecutingXPathGetsOneElement(doc, h)    
+
+    def __assertExecutingXPathGetsOneElement(self, doc, xpath):
+        x = doc.findall(xpath)
+        self.assertEquals(1, len(x))
+                
+    def __assertFormatDownloadSectionAnchorTagHasCorrectText(self, sectionClass, formatCode):
+        doc = self.__GetXmlFromBookHandlerForBookWithAllFormats()
+        t = doc.findall("./body/div[@class='downloads']/p[@class='%s']/a" % sectionClass)[0].text
+        self.assertEquals(formatCode, t)
+    
     def __GetXmlFromBookHandlerForBookWithAllFormats(self):
         return etree.fromstring(self.__handler.Handle("1"))
-    
-    
-    
-    
