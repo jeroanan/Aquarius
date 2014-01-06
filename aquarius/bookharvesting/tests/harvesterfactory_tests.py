5b9d597a1e5014549d5ecbe95c22dd5d51079a5a
Dave Wilson <david.tosslehoff@gmail.com>
2014-01-06 22:18:25 +0000

    upped coverage

--- aquarius/bookharvesting/tests/harvesterfactory_tests.py
+++ aquarius/bookharvesting/tests/harvesterfactory_tests.py
@@ -1,13 +1,19 @@
+from aquarius.bookharvesting.filesystemharvester import filesystemharvester
 from aquarius.bookharvesting.hardcodedharvester import hardcodedharvester
 from aquarius.bookharvesting.harvesterfactory import harvesterfactory
 
 import unittest
+
 
 class harversterfactory_tests(unittest.TestCase):
     
     def setUp(self):
         self.__f = harvesterfactory(None, None)
         
-    def testGetHardodedHarvester(self):
+    def testGetHardcodedHarvester(self):
         h = self.__f.GetHarvester("moo")
         self.assertIsInstance(h, hardcodedharvester)
+
+    def testGetFilesystemHarvester(self):
+        h = self.__f.GetHarvester("filesystem")
+        self.assertIsInstance(h, filesystemharvester)
