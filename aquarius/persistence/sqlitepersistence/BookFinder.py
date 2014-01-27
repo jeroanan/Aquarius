7eeea5bcb86c4246de0fbbb17dc5aa8f4ba4517e
Dave Wilson <david.tosslehoff@gmail.com>
2014-01-27 23:20:32 +0000

    Fixed book search (bah!)

--- aquarius/persistence/sqlitepersistence/BookFinder.py
+++ aquarius/persistence/sqlitepersistence/BookFinder.py
@@ -1,7 +1,14 @@
 from aquarius.objects.Book import Book
+from aquarius.objects.bookformat import bookformat
+from aquarius.persistence.sqlitepersistence.ParameterSanitiser \
+    import ParameterSanitiser
 
 
 class BookFinder(object):
+
+    def __init__(self):
+        self.sanitiser = ParameterSanitiser()
+        self.__connection = None
 
     def convert_search_results_to_books(self, search_result):
         books = []
@@ -14,3 +21,23 @@ class BookFinder(object):
         b.id, b.title, b.author = result
         self.__add_formats_to_book(b)
         books.append(b)
+
+    def __add_formats_to_book(self, a_book):
+        formats = self.__get_formats_for_book(a_book)
+        for f in formats:
+            self.__add_book_to_format(a_book, f)
+
+    def __get_formats_for_book(self, b):
+        (i,) = self.sanitiser.sanitise((b.id,))
+        sql = "SELECT Format, Location FROM BookFormat WHERE Book=%s" % i
+        formats = self.connection.execute_sql_fetch_all(sql)
+        return formats
+
+    @staticmethod
+    def __add_book_to_format(a_book, book_format):
+        bf = bookformat()
+        bf.Format, bf.Location = book_format
+        a_book.formats.append(bf)
+
+    def set_parameter_sanitiser(self, sanitiser):
+        self.sanitiser = sanitiser
--- aquarius/persistence/sqlitepersistence/SearchBook.py
+++ aquarius/persistence/sqlitepersistence/SearchBook.py
@@ -1,17 +1,14 @@
-from aquarius.objects.bookformat import bookformat
 from aquarius.persistence.sqlitepersistence.BookFinder import BookFinder
-from aquarius.persistence.sqlitepersistence.ParameterSanitiser \
-    import ParameterSanitiser
 
 
 class SearchBook(BookFinder):
 
     def __init__(self):
-        self.__connection = None
-        self.__sanitiser = ParameterSanitiser()
+        self.connection = None
+        super().__init__()
 
     def search_books(self, search_term, connection):
-        self.__connection = connection
+        self.connection = connection
         search_term = "%s%s%s" % ("%", search_term, "%")
         search_result = self.__do_search(search_term)
         return self.convert_search_results_to_books(search_result)
@@ -24,11 +21,11 @@ class SearchBook(BookFinder):
         return search_result
     
     def __search_by_title(self, search_term):
-        (st) = self.__sanitiser.sanitise(search_term)
+        (st) = self.sanitiser.sanitise(search_term)
         sql = """SELECT b.Id, b.Title, b.Author
                FROM Book as b 
-               WHERE Title LIKE '%s';""" % st
-        return self.__connection.execute_sql_fetch_all(sql)
+               WHERE Title LIKE '%s';""" % list(st)[0]
+        return self.connection.execute_sql_fetch_all(sql)
     
     def __append_search_result(self, result_set, search_result):
         new_list = []
@@ -43,28 +40,10 @@ class SearchBook(BookFinder):
                 new.append(element)
 
     def __search_by_author(self, search_term):
-        (st) = self.__sanitiser.sanitise(search_term)
+        (st) = self.sanitiser.sanitise(search_term)
         sql = """SELECT b.Id, b.Title, b.Author
                  FROM Book as b
                  WHERE Author LIKE '%s';""" % st
-        return self.__connection.execute_sql_fetch_all(sql)
-    
-    def __add_formats_to_book(self, a_book):
-        formats = self.__get_formats_for_book(a_book)
-        for f in formats:
-            self.__add_book_to_format(a_book, f)
-    
-    def __get_formats_for_book(self, b):
-        (i,) = self.__sanitiser.sanitise((b.id,))
-        sql = "SELECT Format, Location FROM BookFormat WHERE Book=%s" % i
-        formats = self.__connection.execute_sql_fetch_all(sql)
-        return formats
-                    
-    @staticmethod
-    def __add_book_to_format(a_book, book_format):
-        bf = bookformat()
-        bf.Format, bf.Location = book_format
-        a_book.formats.append(bf)
+        return self.connection.execute_sql_fetch_all(sql)
 
-    def set_parameter_sanitiser(self, sanitiser):
-        self.__sanitiser = sanitiser
+
