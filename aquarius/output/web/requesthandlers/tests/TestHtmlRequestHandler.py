58edc16aa3f759f4b3646fb374024ab3c1dc74a8
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-01 23:40:45 +0000

    --pydoc

--- aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandler.py
+++ aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandler.py
@@ -9,44 +9,35 @@ from aquarius.output.web.requesthandlers.tests.Mocks.HtmlRequestHandlerDelegateS
 
 
 class TestHtmlRequestHandler(unittest.TestCase):
-    """Tests for the HtmlRequestHandler class"""
+
     def setUp(self):
-        """Common setup operations"""
         self.__a = Aquarius("hardcoded", None, None)
         self.__a.harvest_books = Mock(return_value=None)
         self.__spy = HtmlRequestHandlerDelegateSpy()
         self.__h = HtmlRequestHandler(self.__a)
         
     def testIndexHandlerReturnsHtmlDocument(self):
-        """Given a request for the index page, then return a html document"""
         self.__AssertIsHtmlDoc(self.__h.index_handler())
 
     def testSearchHandlerCallsSearchHandlerObject(self):
-        """Given a request for a search, then the search handler object is called"""
         self.__h.set_search_handler(self.__spy)
         self.__h.search_handler("searchTerm")
         self.assertTrue(self.__spy.handle_called)
 
     def testHarvestHandlerCallsHarvestBooks(self):
-        """Given a request to harvest books, then call the harvester method"""
         self.__h.harvest_handler()
         self.assertTrue(self.__a.harvest_books.called)
 
     def testBookHandlerCallsBookHandlerObject(self):
-        """Given a request for book details, then the book details
-        handler object is called"""
         self.__h.set_book_handler(self.__spy)
         self.__h.book_handler("1")
         self.assertTrue(self.__spy.handle_called)
     
     def testDownloadHandlerReturnsSomething(self):
-        """Given a download request, then download the file"""
         b = self.__h.download_handler("1", "EPUB")
         self.assertGreater(len(b), 0)
 
     def testFirstLetterHandlerReturnsHtmlDocument(self):
-        """Given a request to list books by first letter, then the first
-        letter handler object is called."""
         self.__h.set_first_letter_handler(self.__spy)
         self.__h.first_letter_handler("T")
         self.assertTrue(self.__spy.handle_called)
