2f86d58b9993e0ba0dcdbf57814b9167e484fa8b
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-01 23:22:12 +0000

    Move to mocking framework here.

--- aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerSearch.py
+++ aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerSearch.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 
 from aquarius.Aquarius import Aquarius
 from aquarius.output.web.requesthandlers.HtmlRequestHandlerSearch import HtmlRequestHandlerSearch
@@ -8,34 +9,23 @@ from aquarius.output.web.requesthandlers.SearchTemplateHandler import SearchTemp
 class TestHtmlRequestHandlerSearch(unittest.TestCase):
 
     def setUp(self):
-        self.__app = AppSpy()
-        self.__search_template_handler = SearchTemplateHandlerSpy()
+        self.InitialiseMockApp()
+        self.InitialiseMockSearchTemplateHandler()
         self.__h = HtmlRequestHandlerSearch(self.__app)
         self.__h.set_search_template_handler(self.__search_template_handler)
 
+    def InitialiseMockSearchTemplateHandler(self):
+        self.__search_template_handler = SearchTemplateHandler()
+        self.__search_template_handler.render_search_template = Mock(return_value=None)
+
+    def InitialiseMockApp(self):
+        self.__app = Aquarius("hardcoded", None, None)
+        self.__app.search_books = Mock(return_value=[])
+
     def test_handle_calls_search(self):
         self.__h.handle("test")
-        self.assertEquals(1, self.__app.search_books_called)
+        self.assertEquals(1, self.__app.search_books.called)
 
     def test_handle_calls_template_handler(self):
         self.__h.handle("test")
-        self.assertEquals(1, self.__search_template_handler.render_search_template_called)
-
-class AppSpy(Aquarius):
-
-    def __init__(self):
-        super(AppSpy, self).__init__("hardcoded", None, None)
-        self.search_books_called = 0
-
-    def search_books(self, searchterm):
-        self.search_books_called += 1
-        return []
-
-
-class SearchTemplateHandlerSpy(SearchTemplateHandler):
-
-    def __init__(self):
-        self.render_search_template_called = 0
-
-    def render_search_template(self, search_results):
-        self.render_search_template_called += 1
+        self.assertEquals(1, self.__search_template_handler.render_search_template.called)
