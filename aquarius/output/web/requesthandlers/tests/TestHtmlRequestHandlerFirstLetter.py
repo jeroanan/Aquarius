94709ca768d3efd0786af044beea8dad101ff1a1
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-01 23:22:47 +0000

    --pydoc

--- aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerFirstLetter.py
+++ aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerFirstLetter.py
@@ -6,15 +6,12 @@ from aquarius.output.web.requesthandlers.HtmlRequestHandlerFirstLetter import Ht
 
 
 class TestHtmlRequestHandlerFirstLetter(unittest.TestCase):
-    """Unit tests for the HtmlRequestHandlerFirstLetter class"""
+
     def setUp(self):
-        """Common setup operations"""
         self.__a = Aquarius("hardcoded", None, None)
         self.__h = HtmlRequestHandlerFirstLetter(self.__a)
             
     def testFirstLetterHandlerCallsApplication(self):
-        """Given a call to the first letter handler's handle method,
-        then call the application's first letter handler method"""
         self.__a.list_books_by_first_letter = Mock(return_value=[])
         self.__h.handle("t")
         self.assertTrue(self.__a.list_books_by_first_letter.called)
