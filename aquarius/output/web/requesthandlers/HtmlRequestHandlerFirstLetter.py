71a59d1c7d9ec857aac8bb04c3b547203e4af5af
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-01 23:38:29 +0000

    Sharpened some tests up

--- aquarius/output/web/requesthandlers/HtmlRequestHandlerFirstLetter.py
+++ aquarius/output/web/requesthandlers/HtmlRequestHandlerFirstLetter.py
@@ -3,12 +3,14 @@ from aquarius.output.web.requesthandlers.SearchTemplateHandler \
 
 
 class HtmlRequestHandlerFirstLetter(object):
-    """Handles html requests to list all books beginning with a given letter"""
-    def __init__(self, app):
-        """Set initial object state"""
-        self.__app = app
 
-    def handle(self, firstletter):
-        """Handle the request"""
-        r = list(self.__app.list_books_by_first_letter(firstletter))
-        return SearchTemplateHandler().render_search_template(r)
+    def __init__(self, app):
+        self.__app = app
+        self.__search_template_handler = SearchTemplateHandler()
+
+    def handle(self, first_letter):
+        r = list(self.__app.list_books_by_first_letter(first_letter))
+        return self.__search_template_handler.render_search_template(r)
+
+    def set_search_template_handler(self, handler):
+        self.__search_template_handler = handler
--- aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerFirstLetter.py
+++ aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerFirstLetter.py
@@ -3,15 +3,29 @@ from unittest.mock import Mock
 
 from aquarius.Aquarius import Aquarius
 from aquarius.output.web.requesthandlers.HtmlRequestHandlerFirstLetter import HtmlRequestHandlerFirstLetter
+from aquarius.output.web.requesthandlers.SearchTemplateHandler import SearchTemplateHandler
 
 
 class TestHtmlRequestHandlerFirstLetter(unittest.TestCase):
 
     def setUp(self):
-        self.__a = Aquarius("hardcoded", None, None)
+        self.initialise_mock_app()
+        self.initialise_mock_search_template_handler()
         self.__h = HtmlRequestHandlerFirstLetter(self.__a)
+        self.__h.set_search_template_handler(self.__search_template_handler)
+
+    def initialise_mock_app(self):
+        self.__a = Aquarius("hardcoded", None, None)
+        self.__a.list_books_by_first_letter = Mock(return_value=[])
+
+    def initialise_mock_search_template_handler(self):
+        self.__search_template_handler = SearchTemplateHandler()
+        self.__search_template_handler.render_search_template = Mock(return_value=None)
             
     def testFirstLetterHandlerCallsApplication(self):
-        self.__a.list_books_by_first_letter = Mock(return_value=[])
         self.__h.handle("t")
         self.assertTrue(self.__a.list_books_by_first_letter.called)
+
+    def test_first_letter_handler_calls_search_template_handler(self):
+        self.__h.handle("t")
+        self.assertTrue(self.__search_template_handler.render_search_template.called)
