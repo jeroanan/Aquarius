a0d2ef3d2944199414dbccf293882d977ca201ef
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-01 23:51:29 +0000

    Improved test coverage

--- BootStrapper.py
+++ BootStrapper.py
@@ -4,11 +4,15 @@ from aquarius.Aquarius import Aquarius
 
 
 class BootStrapper(object):
-    
-    @staticmethod
-    def main():
-        a = Aquarius("sqlite", "web", "filesystem")
-        a.main()
+
+    def __init__(self):
+        self.__a = Aquarius("sqlite", "web", "filesystem")
+
+    def main(self):
+        self.__a.main()
+
+    def set_app(self, app):
+        self.__a = app
               
 if __name__=="__main__":
     BootStrapper().main()
--- tests/bootstrapper_tests.py
+++ tests/bootstrapper_tests.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 
 import unittest
+from unittest.mock import Mock
+
+from aquarius.Aquarius import Aquarius
 from BootStrapper import BootStrapper
 
 
 class bootstrapper_tests(unittest.TestCase):
-    
-    @staticmethod
-    def testCanInitialise():
-        a = BootStrapper()
+
+    def setUp(self):
+        self.initialise_mock_app()
+        self.__boot = BootStrapper()
+        self.__boot.set_app(self.__app)
+
+    def initialise_mock_app(self):
+        self.__app = Aquarius("hardcoded", None, None)
+        self.__app.main = Mock(return_value=None)
+
+    def test_calling_main_calls_main(self):
+        self.__boot.set_app(self.__app)
+        self.__boot.main()
+        self.assertTrue(self.__app.main.called)
