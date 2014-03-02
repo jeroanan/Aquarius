cf228825657b83ba181d9bc2a6d0aa2a190dac39
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-02 20:51:15 +0000

    cleanup

--- aquarius/output/web/requesthandlers/HtmlRequestHandlerDownload.py
+++ aquarius/output/web/requesthandlers/HtmlRequestHandlerDownload.py
@@ -7,5 +7,8 @@ class HtmlRequestHandlerDownload(object):
         book = self.__app.get_book_details(book_id)
         for thisFormat in book.formats:
             if thisFormat.Format == format_code:
-                with open(thisFormat.Location, 'r') as f:
-                    return f.read()
+                return self.get_file_contents(thisFormat)
+
+    def get_file_contents(self, format):
+        with open(format.Location, 'r') as f:
+            return f.read()
