956f77a1a2c5d0092fa558bf6bd32e01123a175a
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-11 21:12:00 +0000

    Fix #12. PdfReaderSpy will be staying for now because it seems that PDFReader is going to be a little tricky to construct without a stream object. Even for mocking purposes.

--- aquarius/bookformats/tests/TestBookFactory.py
+++ aquarius/bookformats/tests/TestBookFactory.py
@@ -1,7 +1,9 @@
 import unittest
+from unittest.mock import Mock
 
 from aquarius.bookformats.BookFactory import BookFactory
-from aquarius.bookformats.tests.epubcreatorspy import EpubCreatorSpy
+from aquarius.bookformats.epubcreator import EpubCreator
+from aquarius.bookformats.pdfcreator import PdfCreator
 
 
 class TestBookFactory(unittest.TestCase):
@@ -16,16 +18,18 @@ class TestBookFactory(unittest.TestCase):
         self.assertIsNone(b)
 
     def testGetEpubCausesCallToEpubCreatorCreate(self):
-        spy = EpubCreatorSpy()
-        self.__f.epub_creator = spy
+        epub_creator = EpubCreator()
+        epub_creator.create = Mock()
+        self.__f.epub_creator = epub_creator
         self.__f.get_book(self.__epubPath)
-        self.assertTrue(spy.createcalled)
+        self.assertTrue(epub_creator.create.called)
 
     def testGetPdfCausesCallToPdfCreatorCreate(self):
-        spy = EpubCreatorSpy()
-        self.__f.pdf_creator = spy
+        pdf_creator = PdfCreator()
+        pdf_creator.create = Mock()
+        self.__f.pdf_creator = pdf_creator
         self.__f.get_book(self.__pdfpath)
-        self.assertTrue(spy.createcalled)
+        self.assertTrue(pdf_creator.create.called)
 
     def test_epub_creator_property(self):
         self.__f.epub_creator = "moo"
--- aquarius/bookformats/tests/TestPdf.py
+++ aquarius/bookformats/tests/TestPdf.py
@@ -1,31 +1,26 @@
 import unittest
+from unittest.mock import Mock
+from PyPDF2 import PdfFileReader
 
 from aquarius.bookformats.Pdf import Pdf
 from aquarius.bookformats.tests.pdfreaderspy import PdfReaderSpy
 
 
 class TestPdf(unittest.TestCase):
-    """Tests for the Pdf class"""
+
     def setUp(self):
-        """Common setup operations"""
         self.__p = Pdf("aquarius/bookformats/tests/data/1.pdf")
 
     def testCallingLoadCallsPdfReaderGetDocumentInfo(self):
-        """Given a book, when the load method is called,
-        then the PdfReader's get_document_info method is called."""
-        spy = PdfReaderSpy(None)
-        self.__p.set_pdf_reader(spy)
+        pdf_reader = PdfReaderSpy(None)
+        self.__p.set_pdf_reader(pdf_reader)
         self.__p.load()
-        self.assertTrue(spy.get_document_info_called)
+        self.assertTrue(pdf_reader.get_document_info_called)
 
     def testCallingLoadSetsCorrectAuthor(self):
-        """Given a book, when the load method is called,
-        then the returned book's author is correctly set."""
         self.__p.load()
         self.assertEquals("Robert Louis Stevenson", self.__p.author)
 
     def testCallingLoadSetsCorrectTitle(self):
-        """Given a book, when the load method is called,
-        then the returned book's title is correctly set."""
         self.__p.load()
         self.assertEquals("Treasure Island", self.__p.title)
