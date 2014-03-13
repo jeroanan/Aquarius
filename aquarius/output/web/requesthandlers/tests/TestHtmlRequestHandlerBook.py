1de1916e5726ccd7d9a408600e1ce2f5139b03ee
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-13 23:10:11 +0000

    Fix #16. TestHtmlRequestHandlerBook.py is less fragile.

--- aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerBook.py
+++ aquarius/output/web/requesthandlers/tests/TestHtmlRequestHandlerBook.py
@@ -1,21 +1,20 @@
 import unittest
+from unittest.mock import Mock
 
 from aquarius.Aquarius import Aquarius
+from aquarius.objects.Book import Book
 from aquarius.output.web.requesthandlers.HtmlRequestHandlerBook import HtmlRequestHandlerBook
 
 
 class TestHtmlRequestHandlerBook(unittest.TestCase):
-    """Tests for the HtmlRequestHandlerBook class"""
+
     def setUp(self):
-        """Common setup operations"""
-        self.__testBookTitle = "The Book with no name"
-        self.__testBookAuthor = "An Author"
-        self.__handler = HtmlRequestHandlerBook(Aquarius("hardcoded", None, None))
+        self.__app = Aquarius("hardcoded", None, None)
+        self.__app.get_book_details = Mock(return_value=(Book()))
+        self.__book_handler = HtmlRequestHandlerBook(self.__app)
         
     def testBookHandlerReturnsHtmlDocument(self):
-        """Given a Handle call on the Book Handler,
-        then return a html document"""
-        self.__AssertIsHtmlDoc(self.__handler.handle("1"))
+        self.__AssertIsHtmlDoc(self.__book_handler.handle("1"))
 
     def __AssertIsHtmlDoc(self, test_string):
         return self.assertTrue(test_string.startswith("<!DOCTYPE html>"))
