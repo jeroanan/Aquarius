e161867cc55a3e5221be5939caea33ac43284534
Dave Wilson <david.tosslehoff@gmail.com>
2014-03-25 23:32:39 +0000

    Separated out GetExistingBookId. Got rid of an instance of null propagating in the system.

--- aquarius/persistence/sqlitepersistence/AddBook.py
+++ aquarius/persistence/sqlitepersistence/AddBook.py
@@ -1,11 +1,14 @@
+from aquarius.persistence.sqlitepersistence.GetExistingBookId import GetExistingBookId
+
+
 class AddBook(object):
 
     def __init__(self, connection):
         self.__connection = connection
 
     def add_book(self, book):
-        book_id = self.__get_existing_book_id(book)
-        if book_id is None:
+        book_id = GetExistingBookId(self.__connection).get_existing_book_id(book)
+        if book_id == -1:
             book.id = self.__add_new_book_returning_its_id(book)
         else:
             book.id = book_id
@@ -15,12 +18,6 @@ class AddBook(object):
         sql = "INSERT INTO Book (Title, Author) VALUES (?, ?)"
         self.__connection.execute_sql_with_params(sql, (book.title, book.author))
         return self.__connection.get_last_row_id()
-
-    def __get_existing_book_id(self, book):
-        sql = "SELECT Id FROM Book WHERE Title=? AND Author=?"
-        r = list(self.__connection.execute_sql_fetch_all_with_params(sql, (book.title, book.author)))
-        if len(r) > 0:
-            return r[0][0]
 
     def __add_book_formats(self, book):
         for f in book.formats:
---
+++ tests/persistence/sqlitepersistence/TestGetExistingBookId.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import Mock
+from aquarius.objects.Book import Book
+from aquarius.persistence.sqlitepersistence.Connection import Connection
+from aquarius.persistence.sqlitepersistence.GetExistingBookId import GetExistingBookId
+
+
+class TestGetExistingBookId(unittest.TestCase):
+
+    def test_initialise(self):
+        o = GetExistingBookId(Mock(Connection))
+
+    def test_book_exists_returns_book(self):
+        conn = Mock(Connection)
+        conn.execute_sql_fetch_all_with_params = lambda x, y: [[0]]
+        o = GetExistingBookId(conn)
+
+        book_id = o.get_existing_book_id(Book())
+        self.assertEquals(0, book_id)
+
+    def test_book_exists_returns_minus_one(self):
+        conn = Mock(Connection)
+        conn.execute_sql_fetch_all_with_params = lambda x, y: []
+        o = GetExistingBookId(conn)
+
+        book_id = o.get_existing_book_id(Book())
+        self.assertEquals(-1, book_id)
