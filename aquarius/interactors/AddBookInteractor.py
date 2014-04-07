893261d4424a96a3e9e4ca6ad6f68a57e81ea75d
Dave Wilson <david.tosslehoff@gmail.com>
2014-04-07 22:18:14 +0100

    Finished AddBookInteractor

--- aquarius/interactors/AddBookInteractor.py
+++ aquarius/interactors/AddBookInteractor.py
@@ -5,5 +5,18 @@ class AddBookInteractor(object):
 
     def execute(self, book):
         b = self.__persistence.get_book_by_title_and_author(book)
-        if b.id == "":
+        if not self.__book_exists(b):
             self.__persistence.add_book(book)
+            b = self.__persistence.get_book_by_title_and_author(book)
+        self.__add_book_formats(b)
+
+    def __add_book_formats(self, b):
+        for f in b.formats:
+            if self.__format_does_not_exist(b, f):
+                self.__persistence.add_book_format(b.id, f)
+
+    def __book_exists(self, book):
+        return book.id != ""
+
+    def __format_does_not_exist(self, book, book_format):
+        return not self.__persistence.format_exists(book.id, book_format)
--- tests/interactors/TestAddBookInteractor.py
+++ tests/interactors/TestAddBookInteractor.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import Mock
 from aquarius.interactors.AddBookInteractor import AddBookInteractor
 from aquarius.objects.Book import Book
+from aquarius.objects.BookFormat import BookFormat
 from aquarius.persistence.sqlitepersistence.SqlitePersistence import SqlitePersistence
 
 
@@ -9,6 +10,7 @@ class TestAddBookInteractor(unittest.TestCase):
 
     def test_execute_gets_book_from_persistence(self):
         persistence = Mock(SqlitePersistence)
+        persistence.get_book_by_title_and_author = Mock(return_value=(self.__get_book()))
         target = AddBookInteractor(persistence)
         target.execute(Book())
         self.assertTrue(persistence.get_book_by_title_and_author.called)
@@ -21,10 +23,37 @@ class TestAddBookInteractor(unittest.TestCase):
         self.assertTrue(persistence.add_book.called)
 
     def test_execute_with_existing_book_does_not_add_it(self):
+        persistence = Mock(SqlitePersistence)
+        persistence.get_book_by_title_and_author = Mock(return_value=(self.__get_book()))
+        target = AddBookInteractor(persistence)
+        target.execute(self.__get_book())
+        self.assertFalse(persistence.add_book.called)
+
+    def test_execute_with_format_adds_format(self):
+        persistence = Mock(SqlitePersistence)
+        persistence.get_book_by_title_and_author = Mock(return_value=(self.__get_book_with_format()))
+        persistence.format_exists = Mock(return_value=False)
+        target = AddBookInteractor(persistence)
+        target.execute(self.__get_book_with_format())
+        self.assertTrue(persistence.add_book_format.called)
+
+    def test_execute_with_format_does_not_add_it_when_the_book_has_it(self):
+        persistence = Mock(SqlitePersistence)
+        persistence.get_book_by_title_and_author = Mock(return_value=(self.__get_book_with_format()))
+        persistence.format_exists = Mock(return_value=True)
+        target = AddBookInteractor(persistence)
+        target.execute(self.__get_book_with_format())
+        self.assertFalse(persistence.add_book_format.called)
+
+    def __get_book_with_format(self):
+        b = self.__get_book()
+        bf = BookFormat()
+        bf.Format = "EPUB"
+        bf.Location = "/dev/null"
+        b.add_format(bf)
+        return b
+
+    def __get_book(self):
         my_book = Book()
         my_book.id = 1337
-        persistence = Mock(SqlitePersistence)
-        persistence.get_book_by_title_and_author = Mock(return_value=my_book)
-        target = AddBookInteractor(persistence)
-        target.execute(my_book)
-        self.assertFalse(persistence.add_book.called)
+        return my_book
