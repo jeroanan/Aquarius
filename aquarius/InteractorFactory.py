f1d4cb15b252e3841c40a0a887cf4bddf151fd11
Dave Wilson <david.tosslehoff@gmail.com>
2014-06-01 00:35:46 +0100

    +"abstract" interactor factory.

---
+++ aquarius/InteractorFactory.py
@@ -0,0 +1,10 @@
+class InteractorFactory(object):
+
+    def get_add_book_interactor(self, persistence):
+        pass
+
+    def get_list_books_by_first_letter_interactor(self, persistence):
+        pass
+
+    def get_search_book_interactor(self, persistence):
+        pass
---
+++ tests/TestInteractorFactory.py
@@ -0,0 +1,17 @@
+import unittest
+from aquarius.InteractorFactory import InteractorFactory
+
+
+class TestInteractorFactory(unittest.TestCase):
+
+    def setUp(self):
+        self.__f = InteractorFactory()
+
+    def test_get_add_book_interactor(self):
+        self.__f.get_add_book_interactor("persistence")
+
+    def test_get_list_books_by_first_letter_interactor(self):
+        self.__f.get_list_books_by_first_letter_interactor("persistence")
+
+    def test_get_search_book_interactor(self):
+        self.__f.get_search_book_interactor("persistence")
